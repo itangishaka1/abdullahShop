1c8a97ba26b529bef72068dffa4b02221ee36267
itangishaka1 <abdullahitangishaka@gmail.com>
2022-09-28 11:31:23 -0400

    Add Qty Select and Add To Card Button

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
+import Cart from './pages/Cart'
 const App = () => {
   return (
     <Router>
@@ -12,6 +13,8 @@ const App = () => {
         <Container>
           <Route path='/' exact component={Home} />
           <Route path='/product/:id' component={ProductPage} />
+          <Route path='/cart/:id?' component={Cart} /> 
+          {/* the '?' in path='/cart/:id?' means the id is optional */}
         </Container>
       </main>
       <Footer />
--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import {
@@ -9,25 +9,31 @@ import {
   Card,
   Button,
   ListGroupItem,
+  Form,
 } from 'react-bootstrap'
 import Rating from '../components/Rating/Rating'
 import { productDetails } from '../actions/productActions'
 import Loader from '../components/Loader/Loader'
 import Message from '../components/Message/Message'
 
-const ProductPage = ({ match }) => {
+const ProductPage = ({ history , match }) => {
+  const [qty, setQty] = useState(0)
   const dispatch = useDispatch()
 
-  const product_details = useSelector(state => state.productDetails) // this productDetails is from store.js
+  const product_details = useSelector((state) => state.productDetails) // this productDetails is from store.js
 
-  const { loading, error, product }  = product_details
-
+  const { loading, error, product } = product_details
 
   useEffect(() => {
     dispatch(productDetails(match.params.id))
   }, [dispatch, match])
 
-  if(!product) {
+  const addToCartHandler = () => {
+    history.push(`/cart/${match.params.id}?qty=${qty}`)
+  }
+
+
+  if (!product) {
     return <p>Loading....</p>
   }
   return (
@@ -35,57 +41,81 @@ const ProductPage = ({ match }) => {
       <Link className='btn btn-light my-3' to='/'>
         Go Back
       </Link>
-      {loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : (
-      <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
-          {/* fluid will keep the image inside the container if the image was going out of its container */}
-        </Col>
-        <Col md={3}>
-          <ListGroup variant='flush'>
-            <ListGroup.Item>
-              <h3>{product.name}</h3>
-            </ListGroup.Item>
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={`${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroupItem>Price: ${product.price}</ListGroupItem>
-            <ListGroupItem>Description: {product.description}</ListGroupItem>
-          </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant='danger'>{error}</Message>
+      ) : (
+        <Row>
+          <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid />
+            {/* fluid will keep the image inside the container if the image was going out of its container */}
+          </Col>
+          <Col md={3}>
             <ListGroup variant='flush'>
-              <ListGroupItem>
-                <Row>
-                  <Col>Price:</Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
-                <Row>
-                  <Col>Status:</Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                  </Col>
-                </Row>
-              </ListGroupItem>
-              <ListGroupItem>
-                <div className='d-grid gap-2'>
-                  <Button type='button' disabled={product.countInStock === 0}>
-                    Add To Cart
-                  </Button>
-                </div>
-              </ListGroupItem>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={`${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroupItem>Price: ${product.price}</ListGroupItem>
+              <ListGroupItem>Description: {product.description}</ListGroupItem>
             </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+          </Col>
+          <Col md={3}>
+            <Card>
+              <ListGroup variant='flush'>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>
+                      <strong>${product.price}</strong>
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                <ListGroupItem>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </Col>
+                  </Row>
+                </ListGroupItem>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <Row>
+                      <Col>Qty</Col>
+                      <Col>
+                        <Form.Control
+                          as='select'
+                          value={qty}
+                          onChange={(e) => setQty(e.target.value)}
+                        >
+                          {[...Array(product.countInStock).keys()].map((x) => (
+                            <option key={x + 1} value={x + 1}>
+                              {x + 1}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Col>
+                    </Row>
+                  </ListGroup.Item>
+                )}
+                <ListGroupItem>
+                  <div className='d-grid gap-2'>
+                    <Button type='button' disabled={product.countInStock === 0} onClick={addToCartHandler}>
+                      Add To Cart
+                    </Button>
+                  </div>
+                </ListGroupItem>
+              </ListGroup>
+            </Card>
+          </Col>
+        </Row>
       )}
     </>
   )
