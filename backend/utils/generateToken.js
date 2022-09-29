f732167ab6a82fe816b62762f7c45d864061c063
itangishaka1 <abdullahitangishaka@gmail.com>
2022-09-29 10:29:28 -0400

    Generate a JSON Web Token

---
+++ backend/utils/generateToken.js
@@ -0,0 +1,10 @@
+import jwt from "jsonwebtoken";
+// import * as dotenv from 'dotenv'
+
+// dotenv.config()
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '3d' })
+}
+
+export default generateToken
