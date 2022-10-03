f6e2b27006d2667d2e65ed1ba1b98cd5b2a59238
itangishaka1 <abdullahitangishaka@gmail.com>
2022-10-03 15:10:03 -0400

    User LoginPage and functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,19 @@ import Footer from './components/Footer/Footer'
 import Home from './pages/Home'
 import ProductPage from './pages/ProductPage'
 import Cart from './pages/Cart'
+import LoginPage from './pages/LoginPage'
+
 const App = () => {
   return (
     <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Route path='/' exact component={Home} />
+          <Route path='/login' component={LoginPage} />
           <Route path='/product/:id' component={ProductPage} />
           <Route path='/cart/:id?' component={Cart} /> 
           {/* the '?' in path='/cart/:id?' means the id is optional */}
+          <Route path='/' exact component={Home} />
         </Container>
       </main>
       <Footer />
